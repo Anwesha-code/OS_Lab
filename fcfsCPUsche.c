0b2118c96c46f9764b103d37bbcc42fab8e5c37e
Anwesha-code <149088895+Anwesha-code@users.noreply.github.com>
2025-04-23 06:39:01 +0530

    Add files via upload

---
+++ fcfsCPUsche.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n = 4;
+    int process[] = {1, 2, 3, 4};
+    int arrivalTime[] = {0, 1, 2, 3};
+    int burstTime[] = {5, 3, 8, 6};
+
+    int waitingTime[10], turnaroundTime[10];
+    int totalWT = 0, totalTAT = 0;
+
+    waitingTime[0] = 0;
+
+    for (int i = 1; i < n; i++) {
+        waitingTime[i] = 0;
+        for (int j = 0; j < i; j++)
+            waitingTime[i] += burstTime[j];
+        waitingTime[i] -= arrivalTime[i];
+        if (waitingTime[i] < 0) waitingTime[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        turnaroundTime[i] = burstTime[i] + waitingTime[i];
+        totalWT += waitingTime[i];
+        totalTAT += turnaroundTime[i];
+    }
+
+    printf("Process\tArrival\tBurst\tWaiting\tTurnaround\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\n", process[i], arrivalTime[i], burstTime[i], waitingTime[i], turnaroundTime[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", (float)totalWT / n);
+    printf("\nAverage Turnaround Time = %.2f\n", (float)totalTAT / n);
+
+    return 0;
+}
